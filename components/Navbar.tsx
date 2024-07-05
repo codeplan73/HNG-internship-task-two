e03081045d8e076e1e40e228406297ececd0f4bc
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-07-05 12:24:12 +0100

    completed Navbar PC

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,43 +1,79 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 import { RiMenu5Fill } from "react-icons/ri";
 import { RiShoppingCart2Line } from "react-icons/ri";
 import { FiSearch } from "react-icons/fi";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const menuItems = [
-  { label: "Home", path: "/" },
-  { label: "Shop", path: "/shop" },
-  { label: "Designers", path: "/designers" },
-  { label: "About Us", path: "/about" },
-  { label: "Contact", path: "/contact" },
+  { label: "Home", href: "/" },
+  { label: "Shop", href: "/products" },
+  { label: "Designers", href: "/designers" },
+  { label: "About Us", href: "/about" },
+  { label: "Contact", href: "/contact" },
+];
+
+const iconMenu = [
+  {
+    icon: (
+      <RiMenu5Fill className="text-xl text-neutral cursor-pointer hover:text-primaryColor" />
+    ),
+    label: "Menu",
+  },
+  {
+    icon: (
+      <FiSearch className="text-xl text-neutral cursor-pointer hover:text-primaryColor" />
+    ),
+    label: "Search",
+  },
 ];
 
 const Navbar = () => {
+  const currentPath = usePathname();
   return (
-    <nav className="container flex items-center justify-between py-8">
+    <nav className="container flex items-center justify-between py-4 md:py-8 shadow drop-shadow">
       <Logo />
-      <ul className="flex items-center justify-start gap-8">
-        {menuItems.map((item) => (
-          <li key={item.label}>
-            <Link href={item.path}>
-              <Link href={item.path}>{item.label}</Link>
+      <ul className="hidden md:flex items-center justify-start gap-8">
+        {menuItems.map((link) => (
+          <li key={link.label}>
+            <Link
+              href={link.href}
+              className={classnames({
+                "uppercase text-sm hover:text-primaryColor text-primaryColor":
+                  link.href === currentPath,
+                "text-neutral uppercase  text-sm": link.href !== currentPath,
+                "transition-colors": true,
+              })}
+            >
+              {link.label}
             </Link>
           </li>
         ))}
       </ul>
-      <ul className="flex items-center gap-4">
-        <li>
-          <RiMenu5Fill className="text-xl text-neutral cursor-pointer" />
-        </li>
-        <li>
-          <FiSearch className="text-neutral cursor-pointer" />
-        </li>
-        <li>
-          <Link href="/cart">
-            <RiShoppingCart2Line className="text-xl text-neutral cursor-pointer" />
-          </Link>
-        </li>
+      <ul className="hidden md:flex items-center gap-8 text-md">
+        {iconMenu.map((item) => (
+          <li key={item.label}>{item.icon}</li>
+        ))}
+        <Link href="/cart" className="flex relative">
+          <RiShoppingCart2Line className="text-xl text-neutral cursor-pointer hover:text-primaryColor" />
+          <span className="text-primaryColor absolute bg-white rounded-full text-xs -top-2 left-2 px-1">
+            5
+          </span>
+        </Link>
+      </ul>
+
+      <ul className="flex items-center gap-4 text-md">
+        <Link href="/cart" className="flex relative">
+          <RiShoppingCart2Line className="text-xl text-neutral cursor-pointer hover:text-primaryColor" />
+          <span className="text-primaryColor absolute bg-white rounded-full text-xs -top-2 left-2 px-1">
+            5
+          </span>
+        </Link>
+        <RiMenu5Fill className="text-xl md:hidden text-neutral cursor-pointer hover:text-primaryColor" />
       </ul>
     </nav>
   );
